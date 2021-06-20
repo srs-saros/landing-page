265aa8626f0db0c49095e05a5e23a9e3459e3415
Maria Jose <maria.plata01@correo.usa.edu.co>
2021-06-20 16:09:02 -0500

    cleaning project (deleting innecesary files) and creating tests

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world from {process.env.APP_COUNTRY}
         </p>
         <a
           className="App-link"
