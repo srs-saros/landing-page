287b1ae18df7840b57570addb75258b3ba66c417
sebastian-mancipe <smancipeparedes@gmail.com>
2021-06-25 22:50:23 -0500

    Add ESLint and Prettier to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+import logo from './assets/logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world from {process.env.APP_COUNTRY}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const StyledDiv = styled.div`
+  text-align: center;
+`;
+
+const StyledHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const rotatingAnimation = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const StyledImg = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+
+  @media (prefers-reduced-motion: no-preference) {
+    animation: ${rotatingAnimation} infinite 20s linear;
+  }
+`;
+
+export const App: React.FC = () => (
+  <StyledDiv>
+    <StyledHeader>
+      <StyledImg src={logo} alt="logo" />
+      <p>Hello world from {process.env.REACT_APP_COUNTRY}</p>
+    </StyledHeader>
+  </StyledDiv>
+);
 
 export default App;
